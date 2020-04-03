d55f99ebce8a3618cb1a1e9b78aea4026ab53ba6
jguzman180cs <jguzman180cs@gmail.com>
2020-04-03 08:57:18 -0500

    added ability to parse command arguments. add your own command arguments to your running configuration "command arguments"

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,25 @@
 public class MainClass {
+    private static String traceFileName;
+    private static int cacheSizeInKB;
+    private static int blockSize;
+    private static int associativity;
+    private static String replacementPolicy;
+
     public static void main(String[] args){
+        for(int i = 0;i < args.length; i++){
+            if (args[i].equals("-f")){
+                traceFileName = args[i+1];
+            } else if(args[i].equals("-s")){
+                cacheSizeInKB = Integer.parseInt(args[i+1]);
+            } else if(args[i].equals("-b")){
+                blockSize = Integer.parseInt(args[i+1]);
+            } else if(args[i].equals("-a")){
+                associativity = Integer.parseInt(args[i+1]);
+            } else if(args[i].equals("-r")){
+                replacementPolicy = args[i+1];
+            }
+        }
+
         System.out.println("hello wrold");
     }
 }
