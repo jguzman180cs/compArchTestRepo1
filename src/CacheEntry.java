6eca01d92d535709edbd2feb262d1ac0ee5b534b
Seth Chick <SCBionicle@gmail.com>
2020-04-02 19:58:05 -0500

    initial commit

---
+++ src/CacheEntry.java
@@ -0,0 +1,13 @@
+
+
+public class CacheEntry
+{
+    public int tag;
+    public boolean valid;
+
+    public CacheEntry(int tag, boolean valid)
+    {
+        this.tag = tag;
+        this.valid = valid;
+    }
+}
