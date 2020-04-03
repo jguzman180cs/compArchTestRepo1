a91dacb47274e47930bac8fb321799a3f7d085bc
Seth Chick <SCBionicle@gmail.com>
2020-04-03 18:08:50 -0500

    added initial variables

--- src/Cache.java
+++ src/Cache.java
@@ -2,6 +2,11 @@
 
 public class Cache{
 
-
+    /**
+     * First index is cache index, 2nd index is associativity.
+     */
+    public static CacheEntry[][] cache;
+    private static int hits = 0;
+    private static int misses = 0;
 
 }
